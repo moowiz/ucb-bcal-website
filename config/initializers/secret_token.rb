e4e2d0c5a60faf0397dbed63d54b579a69d0bc3e
Stephen Martinis <moowiz2020@gmail.com>
2013-10-17 15:02:57 -0700

    Initial commit, added basic calendar resource

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bcal::Application.config.secret_token = 'ab733942dc329fe3fccf172444117c40ed7e6549d3e53bca5b528d2cdde853a80729ca8421af133f949b9b51e1a4dbc934a61f2921b04dcef7d8e2bf41de0762'
